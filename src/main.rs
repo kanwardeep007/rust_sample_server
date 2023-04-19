e549795d794b9a674698cebca1e81b0eec0f7a8e
Kanwardeep <kanwar.baweja@gmail.com>
2023-04-19 16:26:00 +0100

    chore

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use kanwar_server::approach_1_server::Approach1Server;
 use kanwar_server::common::ServerTrait;
-
 use std::net::TcpListener;
+
 fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:8181").unwrap();
     let approach_1_server = Approach1Server::new(4);
